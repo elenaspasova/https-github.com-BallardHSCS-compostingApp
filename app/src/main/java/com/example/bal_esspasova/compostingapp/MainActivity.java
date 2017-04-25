b1aa4deef4165a7b2e63d50b6ebd1dfac6371371
elenaspasova <BAL_esspasova@seattleschools.org>
2017-04-25 09:34:37 -0700

    Add Buttons that extend to new activities - Success

--- app/src/main/java/com/example/bal_esspasova/compostingapp/MainActivity.java
+++ app/src/main/java/com/example/bal_esspasova/compostingapp/MainActivity.java
@@ -4,18 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button editText;
+    Button editText2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (Button) findViewById(R.id.button);
+        editText2 = (Button) findViewById(R.id.button2);
     }
 
-    public void sendMessage(View view)
-    {
-        Intent intent = new Intent(FromActivity.this, ToActivity.class);
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
