ccd8f590410a8b569e0585d10763dbe4d00d27ab
mlmyka <BAL_mcsheldon@seattleschools.org>
2017-04-28 12:31:22 -0700

    more work with search
    added search function, added search icon to drawable folder and more

---
+++ app/src/main/java/com/example/bal_esspasova/compostingapp/SearchActivity.java
@@ -0,0 +1,10 @@
+package com.example.bal_esspasova.compostingapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bal_mcsheldon on 4/28/2017.
+ */
+
+public class SearchActivity extends AppCompatActivity {
+}
