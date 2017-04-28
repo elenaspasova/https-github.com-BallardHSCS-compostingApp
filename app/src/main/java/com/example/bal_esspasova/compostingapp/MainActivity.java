828f0c3876a77a0781f6bdd00727549a217bc879
mlmyka <BAL_mcsheldon@seattleschools.org>
2017-04-28 09:28:01 -0700

    action bar
    made action bar and started search icon

--- app/src/main/java/com/example/bal_esspasova/compostingapp/MainActivity.java
+++ app/src/main/java/com/example/bal_esspasova/compostingapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.bal_esspasova.compostingapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,8 +19,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
         editText = (Button) findViewById(R.id.button);
         editText2 = (Button) findViewById(R.id.button2);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        return true;
     }
 
     /** Called when the user taps the Send button */
