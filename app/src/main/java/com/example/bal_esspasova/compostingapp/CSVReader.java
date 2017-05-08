aeb879f5895817da40998aa4b63e5eb7c7840bfb
elenaspasova <BAL_esspasova@seattleschools.org>
2017-05-08 09:35:06 -0700

    Work on reading CSV files - unfinished

---
+++ app/src/main/java/com/example/bal_esspasova/compostingapp/CSVReader.java
@@ -0,0 +1,43 @@
+package com.example.bal_esspasova.compostingapp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by bal_esspasova on 5/8/2017.
+ */
+
+public class CSVReader {
+    InputStream inputStream;
+
+    public CSVReader(InputStream is){
+        this.inputStream = is;
+    }
+
+    public List<String[]> read(){
+        List<String[]> resultList = new ArrayList<String[]>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        try{
+            String csvLine;
+            while((csvLine = reader.readLine()) != null) {
+                String[] row = csvLine.split(",");
+                resultList.add(row);
+
+            }
+        }catch(IOException ex){
+            throw new RuntimeException("Error in reading CSV file: " + ex);
+        }finally{
+            try{
+                inputStream.close();
+            }catch (IOException e){
+                throw new RuntimeException("Error while closing input stream: " + e);
+            }
+        }
+        return resultList;
+    }
+}
