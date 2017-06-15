db374f7f150cae3d3d14cf965d95e16784fd02bf
mlmyka <mykalenasheldon@gmail.com>
2017-06-14 19:06:37 -0700

    search work

--- app/src/main/java/com/example/bal_esspasova/compostingapp/newSearchActivity.java
+++ app/src/main/java/com/example/bal_esspasova/compostingapp/newSearchActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.List;
+
 import static android.R.string.ok;
 
 /**
@@ -15,31 +17,69 @@ import static android.R.string.ok;
  */
 
 public class newSearchActivity extends AppCompatActivity {
-    Button   mButton;
+    Button mButton;
     EditText mEdit;
     TextView mText;
 
-    /** Called when the activity is first created. */
+    List<String[]> itemList;
+    int listLocation;
+    Boolean matchItem;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mButton = (Button)findViewById(R.id.enterButton);
-        mEdit   = (EditText)findViewById(R.id.editText);
+        mButton = (Button) findViewById(R.id.enterButton);
+        mEdit = (EditText) findViewById(R.id.editText);
         mText = (TextView) findViewById(R.id.textView);
 
 
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String txt = mEdit.getText().toString(); //Get txt from et when button is clicked
+                mText.setText(txt);
+                searchWord(txt);
 
+            }
+        });
+    }
 
-        mButton.setOnClickListener(new View.OnClickListener()
-                {
-                    public void onClick(View view)
-                    {
-                        String txt = mEdit.getText().toString(); //Get txt from et when button is clicked
-                        mText.setText(txt);
+    public String[] searchWord(String text) {
+
+        if (text != null) {
+            for (int i = 0; i < itemList.size(); i++) {
+                String[] itemArray = itemList.get(i);
+                if (itemArray[0].equals(text)) {
+                    matchItem = true;
+                    listLocation = i;
+                    break;
+                }
+            }
+            //user searched for an item
+            if (matchItem) {
+                return itemList.get(listLocation);
+                //display that items info based off the value of listLocation
+            } else {
+                for (int i = 0; i < itemList.size(); i++) {
+                    String[] itemArray = itemList.get(i);
+                    for (int j = 0; j < itemArray.length - 1; j++) {
+                        //user searched for a restaurant
+                        if (itemArray[2].equals(text)) {
+                            matchItem = true;
+                            listLocation = i;
+                            break;
+                        }
 
                     }
-                });
+                }
+                if(matchItem){
+
+                }
+            }
+        }
+        return itemList.get(listLocation);
     }
 }
